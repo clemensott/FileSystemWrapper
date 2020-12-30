c1d7e0b76e06509993f3c491af57dc3004f79076
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2020-12-30 20:08:34 +0100

    added url-search-params-polyfill to fix webpage for W10M Edge

--- FileSystemWeb/ClientApp/src/index.js
+++ FileSystemWeb/ClientApp/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import '@fortawesome/fontawesome-free/css/solid.min.css';
+import 'url-search-params-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
