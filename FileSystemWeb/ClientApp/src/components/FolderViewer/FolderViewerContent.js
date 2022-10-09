56ee116b8051192df87cf42b05953760c0c3cb7f
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2022-10-09 16:32:23 +0200

    improve react rendering performance of FolderViewer

---
+++ FileSystemWeb/ClientApp/src/components/FolderViewer/FolderViewerContent.js
@@ -0,0 +1,99 @@
+﻿import React, { memo, useEffect, useState } from 'react';
+import FolderViewerItems from './FolderViewerItems';
+import Loading from '../Loading/Loading';
+
+
+const FolderViewerContent = ({ content, maxItemsCount, paddingTop, onItemCountChanged, onDeleteItem }) => {
+    const [itemGroups, setItemGroups] = useState([]);
+
+    const createItemGroup = (startIndex, maxLength) => {
+        const items = [];
+        for (let i = startIndex; i < content.folders.length; i++) {
+            if (items.length >= maxLength) break;
+            items.push(content.folders[i]);
+        }
+        for (let i = startIndex - content.folders.length + items.length; i < content.files.length; i++) {
+            if (items.length >= maxLength) break;
+            items.push(content.files[i]);
+        }
+
+        return {
+            startIndex,
+            items,
+        };
+    };
+
+    const itemsCount = itemGroups.map(group => group.items.length).reduce((sum, length) => sum + length, 0);
+
+    useEffect(() => {
+        if (!content) {
+            return;
+        }
+        let currentCount = itemsCount;
+        if (currentCount < maxItemsCount) {
+            setItemGroups([
+                ...itemGroups,
+                createItemGroup(currentCount, maxItemsCount - currentCount),
+            ]);
+        } else if (currentCount > maxItemsCount) {
+            setItemGroups(itemGroups.map(group => {
+                if (group.startIndex + group.items.length <= maxItemsCount) {
+                    return group;
+                }
+                if (group.startIndex >= maxItemsCount) {
+                    return null;
+                }
+                return {
+                    startIndex,
+                    items: items.filter((_, index) => index + startIndex <= maxItemsCount),
+                };
+            }).filter(Boolean));
+        }
+    }, [maxItemsCount]);
+
+    useEffect(() => {
+        const groups = [];
+        if (content) {
+            groups.push(createItemGroup(0, maxItemsCount));
+        }
+        setItemGroups(groups);
+    }, [content]);
+
+    useEffect(() => {
+        if (typeof onItemCountChanged === 'function') {
+            onItemCountChanged(itemsCount);
+        }
+    }, [itemsCount]);
+
+    const path = content && content.path.length ? content.path[content.path.length - 1].path : '';
+    const totalItemsCount = content ? content.folders.length + content.files.length : 0;
+
+    return (
+        <>
+            <div className="folder-viewer-list" style={{
+                paddingTop,
+            }}>
+                {
+                    itemGroups.map(group => (
+                        <FolderViewerItems
+                            key={group.startIndex}
+                            path={path}
+                            items={group.items}
+                            onDeleteItem={onDeleteItem}
+                        />
+                    ))
+                }
+                {content && totalItemsCount > itemsCount ? (
+                    <div className="text-center m-4">
+                        <Loading />
+                    </div>
+                ) : null}
+            </div>
+            <div className={totalItemsCount ? 'd-none' : 'text-center'}>
+                <h3 className="font-italic">&lt;Empty&gt;</h3>
+            </div>
+        </>
+    );
+};
+
+export default memo(FolderViewerContent);
---
+++ FileSystemWeb/ClientApp/src/components/FolderViewer/FolderViewerItem.js
@@ -0,0 +1,38 @@
+﻿import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import FSItem from '../FSItem/FSItem';
+import FileActionsDropdown from '../FSItem/FileActionsDropdown';
+import FolderActionsDropdown from '../FSItem/FolderActionsDropdown';
+import { getName } from '../../Helpers/Path';
+
+const FolderViewerItem = ({ path, item, onDeleteItem }) => {
+    let link = null;
+    if (item.isFile && item.permission.info) {
+        link = `/?folder=${encodeURIComponent(path)}&file=${encodeURIComponent(getName(item.path))}`;
+    } else if (!item.isFile && item.permission.list) {
+        link = `/?folder=${encodeURIComponent(item.path)}`;
+    }
+
+    return (
+        <div className="p-2 folder-viewer-item-container">
+            <div className={`folder-viewer-file-item-content ${link ? 'folder-viewer-item-container-link' : ''}`}>
+                {link ? (
+                    <Link to={link}>
+                        <FSItem item={item} />
+                    </Link>
+                ) : (
+                    <div>
+                        <FSItem item={item} />
+                    </div>
+                )}
+            </div>
+            {item.isFile ? (
+                <FileActionsDropdown file={item} onDelete={onDeleteItem} />
+            ) : (
+                <FolderActionsDropdown folder={item} onDelete={onDeleteItem} />
+            )}
+        </div>
+    );
+};
+
+export default memo(FolderViewerItem);
