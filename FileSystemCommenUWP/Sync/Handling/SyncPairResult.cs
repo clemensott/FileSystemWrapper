e4a229d754c652f39021d9a67aaa6bee92bc1da8
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2025-12-03 22:39:09 +0100

    fix SyncPairResult for uwp app

--- FileSystemCommenUWP/Sync/Handling/SyncPairResult.cs
+++ FileSystemCommenUWP/Sync/Handling/SyncPairResult.cs
@@ -1,8 +1,21 @@
 ﻿using FileSystemCommon.Models.Sync.Handling;
+using System.Collections.Generic;
 
 namespace FileSystemCommonUWP.Sync.Handling
 {
-    public class SyncPairResult: BaseSyncPairState<SyncPairStateFileModel>
+    public class SyncPairResult : BaseSyncPairState<SyncPairResultFile>
     {
+        public SyncPairResult()
+        {
+
+        }
+
+        public SyncPairResult(IEnumerable<SyncPairResultFile> files)
+        {
+            foreach (SyncPairResultFile file in files)
+            {
+                AddFile(file);
+            }
+        }
     }
 }
