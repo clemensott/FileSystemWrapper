4809e2649a543b19d7dfc3eaa13062c220e95489
Clemens Ott <42971547+shelbyboss@users.noreply.github.com>
2026-02-22 22:15:19 +0100

    add arrow key navigation to FileViewerOverlay.jsx

--- FileSystemWeb/ClientApp/src/components/FileViewerOverlay.jsx
+++ FileSystemWeb/ClientApp/src/components/FileViewerOverlay.jsx
@@ -1,11 +1,42 @@
-﻿import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+﻿import React, {useCallback, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import FileViewer from './FileViewer/FileViewer';
 import './FileViewerOverlay.css';
 
-export default function ({ closeUrl, previousItem, nextItem, ...rest }) {
+export default function ({closeUrl, previousItem, nextItem, ...rest}) {
     const navigate = useNavigate();
+
+    const onKeypressCallback = useCallback((e) => {
+        if (['video', 'audio'].includes(e.target.tagName.toLowerCase())) {
+            return;
+        }
+
+        switch (e.keyCode) {
+            case 37: // left arrow
+                if (previousItem) {
+                    navigate(previousItem.url);
+                }
+                break;
+
+            case 39: // right arrow
+                if (nextItem) {
+                    navigate(nextItem.url);
+                }
+                break;
+
+            case 27: // esc arrow
+                navigate(closeUrl)
+                break;
+        }
+    }, [nextItem, previousItem, closeUrl]);
+
+    useEffect(() => {
+        document.addEventListener('keydown', onKeypressCallback);
+        return () => {
+            document.removeEventListener('keydown', onKeypressCallback);
+        };
+    }, [onKeypressCallback]);
 
     return (
         <div className="file-viewer-overlay" onClick={e => {
@@ -20,18 +51,18 @@ export default function ({ closeUrl, previousItem, nextItem, ...rest }) {
                 <FileViewer theme="dark" onClose={() => navigate(nextItem ? nextItem.url : closeUrl)} {...rest} />
             </div>
             <Link to={closeUrl}>
-                <i className="fas fa-times-circle fa-3x file-viewer-overlay-close" />
+                <i className="fas fa-times-circle fa-3x file-viewer-overlay-close"/>
             </Link>
             {previousItem && (
                 <Link to={previousItem.url}>
                     <i className="fas fa-chevron-circle-left fa-3x file-viewer-overlay-previous"
-                        title={previousItem.title} />
+                       title={previousItem.title}/>
                 </Link>
             )}
             {nextItem && (
                 <Link to={nextItem.url}>
                     <i className="fas fa-chevron-circle-right fa-3x file-viewer-overlay-next"
-                        title={nextItem.title} />
+                       title={nextItem.title}/>
                 </Link>
             )}
         </div>
