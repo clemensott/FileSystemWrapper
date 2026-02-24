8f0328613160b8e4d0a75cb38184ce1380eeea8c
Clemens Ott <42971547+shelbyboss@users.noreply.github.com>
2026-02-24 23:28:40 +0100

    try legacy plugin for vite

--- FileSystemWeb/ClientApp/vite.config.js
+++ FileSystemWeb/ClientApp/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
     server: {
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
     build: {
         outDir: 'build',
         target: 'es6',
